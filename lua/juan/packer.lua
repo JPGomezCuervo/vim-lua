51fa1e4ef8ca1f8eaf824457856f7a4bf03cbf03
jpgomezcuervo <122913492+JPGomezCuervo@users.noreply.github.com>
2023-08-11 13:46:00 +0900

    Add surrounding plugin

--- lua/juan/packer.lua
+++ lua/juan/packer.lua
@@ -3,8 +3,10 @@
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
+
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
+
   -- rose/pine
   use {
     'rose-pine/neovim',
@@ -13,14 +15,17 @@ return require('packer').startup(function(use)
       vim.cmd('colorscheme rose-pine')
     end
   }
+
   -- telescope
   use {
     'nvim-telescope/telescope.nvim', tag = '0.1.0',
     -- or                            , branch = '0.1.x',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
+  
   -- harpoon
   use ("theprimeagen/harpoon")
+
   -- treesitter 
   use {
     'nvim-treesitter/nvim-treesitter',
@@ -32,7 +37,8 @@ return require('packer').startup(function(use)
 	  'VonHeikemen/lsp-zero.nvim',
 	  branch = 'v2.x',
 	  requires = {
-		  -- LSP Support
+
+  -- LSP Support
 		  {'neovim/nvim-lspconfig'},
 		  {'williamboman/mason.nvim'},
 		  {'williamboman/mason-lspconfig.nvim'},
@@ -49,6 +55,7 @@ return require('packer').startup(function(use)
 		  {'rafamadriz/friendly-snippets'},
 	  }
   }
+
   -- autotag
   use {
     "windwp/nvim-autopairs",
@@ -64,5 +71,8 @@ return require('packer').startup(function(use)
     requires = { 'nvim-tree/nvim-web-devicons', opt = true }
   }
   use "lewis6991/gitsigns.nvim"
+
+  -- surrounding
+  use "tpope/vim-surround"
 
 end)
