4d62ae997bf94b68ebbda4c2188ea94b77c5e24b
jpgomezcuervo <122913492+JPGomezCuervo@users.noreply.github.com>
2023-08-11 15:40:49 +0900

    Add split window keymap

--- lua/juan/keymaps.lua
+++ lua/juan/keymaps.lua
@@ -12,3 +12,10 @@ vim.keymap.set("n", "N", "Nzzzv")
 -- copy into the clipboard
 vim.keymap.set({'n', 'x'}, 'cp', '"+y')
 vim.keymap.set({'n', 'x'}, 'cv', '"+p')
+--split window
+vim.keymap.set("n", "<leader>sv", "<C-w>v")
+vim.keymap.set("n", "<leader>sh", "<C-w>s")
+vim.keymap.set("n", "<leader>se", "<C-w>=")
+vim.keymap.set("n", "<leader>sx", "<:close<CR>")
+vim.keymap.set("n", "<leader>nh", ":nohl<CR>")
+vim.keymap.set("n", "<leader>s;", "<C-w>w")
--- lua/juan/packer.lua
+++ lua/juan/packer.lua
@@ -75,4 +75,16 @@ return require('packer').startup(function(use)
   -- surrounding
   use "tpope/vim-surround"
 
+  -- trouble
+  use({
+    "folke/trouble.nvim",
+    config = function()
+      require("trouble").setup {
+        icons = false,
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    end
+  })
 end)
