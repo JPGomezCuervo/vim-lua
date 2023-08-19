0fdb29619ee19c50a6f133b4eae23781529699ca
jpgomezcuervo <122913492+JPGomezCuervo@users.noreply.github.com>
2023-08-19 11:41:03 +0900

    remove trouble.nvim

--- lua/juan/packer.lua
+++ lua/juan/packer.lua
@@ -75,18 +75,6 @@ return require('packer').startup(function(use)
   -- surrounding
   use "tpope/vim-surround"
 
-  -- trouble
-  use({
-    "folke/trouble.nvim",
-    config = function()
-      require("trouble").setup {
-        icons = false,
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-      }
-    end
-  })
   -- comment.nvim
   use {
     'numToStr/Comment.nvim',
